7f3b22b962ccec25ded20cb51ff585eea6f38689
jaganravi4 <jaganravichandran1@gmail.com>
2026-06-22 00:41:22 +0530

    Added signup logic with password encryption

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,9 @@
 "use strict";
 const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+
+const { SALT } = require("../config/serverConfig");
+
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
         static associate(models) {}
@@ -27,5 +31,11 @@ module.exports = (sequelize, DataTypes) => {
             modelName: "User",
         },
     );
+
+    User.beforeCreate((user) => {
+        const encryptedPassword = bcrypt.hashSync(user.password, SALT);
+        user.password = encryptedPassword;
+    });
+
     return User;
 };
---
+++ src/repositories/user-repository.js
@@ -0,0 +1,15 @@
+const { User } = require("../models/index");
+
+class UserRepository {
+    async create(data) {
+        try {
+            const user = await User.create(data);
+            return user;
+        } catch (error) {
+            console.log("Something went wrong in the user repository");
+            throw { error };
+        }
+    }
+}
+
+module.exports = UserRepository;
